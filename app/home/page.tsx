fc91cbecffa0e62602e9e8c1adfcc4fe488954b4
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-16 00:20:06 +0530

    Adjusting the Z-index

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,21 +1,24 @@
-import SignOut from "@/components/sign-out";
 import Toggle from "@/components/Toggle";
 import Profile from "@/components/profile";
 
 const Home = () => {
   return (
-    <main className="relative p-4">
-      <div className="absolute inset-0 top-4 left-4 z-10">
-        <Toggle />
-      </div>
-      <div className="absolute inset-0 top-4 right-4 z-10">
-        <Profile />
+    <main className="items-center justify-center">
+      <div className="relative">
+        <div className="absolute inset-0 top-4 left-4 z-10">
+          <Toggle />
+        </div>
+        <div className="absolute inset-0 top-4 right-4 z-10">
+          <Profile />
+        </div>
       </div>
       <div className="mt-10">
-        <div>
-          <h1>Home</h1>
-          <SignOut />
-        </div>
+        <h1 className="text-3xl font-semibold text-center">
+          Welcome to the Home Page
+        </h1>
+        <p className="text-center mt-2">
+          This is a simple home page with a toggle and profile dropdown.
+        </p>
       </div>
     </main>
   );
