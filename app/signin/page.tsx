fc83cc5ae55e8ee0e8771244b5b740881985f55b
kanugurajesh <kanugurajesh3@gmail.com>
2024-10-15 23:39:50 +0530

    Adding the 0Auth Provider

---
+++ app/signin/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import SignIn from "@/components/sign-in";
+
+const Login = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="bg-white p-6 rounded-md shadow-md">
+        <h1 className="text-2xl mb-4 text-center font-semibold">Sign in</h1>
+        <SignIn provider="Google" />
+        <SignIn provider="GitHub" />
+      </div>
+    </main>
+  );
+};
+
+export default Login;
--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -1,13 +1,26 @@
-import { signIn } from "@/auth";
+"use client";
 
-const SignIn = (provider: string) => {
+// import { signIn } from "@/auth";
+import { signIn } from "next-auth/react";
+
+interface SignInProps {
+  provider: string;
+}
+
+const SignIn = (provider: SignInProps) => {
+  const handleSignIn = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await signIn(provider.provider, {redirectTo: "/home"});
+  };
+
   return (
-    <form
-      action={async () => {
-        await signIn(provider);
-      }}
-    >
-      <button type="submit">Signin with Google</button>
+    <form onSubmit={handleSignIn}>
+      <button
+        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 w-full mb-3"
+        type="submit"
+      >
+        Signin with {provider.provider}
+      </button>
     </form>
   );
 };
